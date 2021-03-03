2ba1820c61f909e469e37e1202a701a07b4775b1
chwnk <48645500+chwnk@users.noreply.github.com>
2021-03-04 00:41:57 +0100

    Zadanie4 finished
    Zadanie4 finished

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var copier = new Copier();
+            var printer = (IPrinter)copier;
+            var scanner = (IScanner)copier;
+
+            copier.CopierPowerOn();
+            Console.WriteLine($"Device {copier.GetState()}, Printer {printer.GetState()}, Scanner {scanner.GetState()}");
+            copier.CopierStandbyOn();
+            Console.WriteLine($"Device {copier.GetState()}, Printer {printer.GetState()}, Scanner {scanner.GetState()}");
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            copier.Print(in doc1);
+            Console.WriteLine($"Device {copier.GetState()}, Printer {printer.GetState()}, Scanner {scanner.GetState()}");
+            copier.Print(in doc1);
+            IDocument doc2;
+            copier.Scan(out doc2);
+            Console.WriteLine($"Device {copier.GetState()}, Printer {printer.GetState()}, Scanner {scanner.GetState()}");
+            copier.Scan(out doc2);
+            copier.ScanAndPrint();
+            Console.WriteLine(copier.Counter);
+            Console.WriteLine(copier.PrintCounter);
+            Console.WriteLine(copier.ScanCounter);
+        }
+    }
+}
